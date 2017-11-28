3063543a488de89f63362d03cdbdc6c53d528788
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 22:11:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  new_array = [] #initiate empty array to keep modified elements
+  i = 0 #set to counter to index position 0
+  while i < array.length #condition
+    new_array.push yield(array[i]) #populate new_array with .yield method
+    i += 1 #increment counter
+  end
+  new_array #return new_array
+end
 
-
+array = [] #initiate empty array
+my_collect(array) do |element| #block with the required code
+  element.upcase
+end
